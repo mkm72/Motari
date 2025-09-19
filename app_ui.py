b83b3df69426ae4eabd64973247a1b7807b6fee6
mkm7 <mkm7294@gmail.com>
2025-09-19 11:40:09 +0300

    add the main ui frame

--- app_ui.py
+++ app_ui.py
@@ -1,1 +1,11 @@
+import customtkinter
 
+class AppUI(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("temp_name_change_me")
+        self.geometry("400x400")
+        
+        
+        
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import app_ui
 
+
+app =app_ui.AppUI()
+app.mainloop()
